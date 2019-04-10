1fdb06361f74ae166de5780d925d8c4961389432
Daniel Sanchez Ceinos <daniel.sanchezc@masmovil.com>
2019-04-10 12:26:09 +0200

    Step 5

--- app/src/main/java/com/danielsanchezc/profiledatabindingexample/BindingAdapters.kt
+++ app/src/main/java/com/danielsanchezc/profiledatabindingexample/BindingAdapters.kt
@@ -1,14 +1,29 @@
 package com.danielsanchezc.profiledatabindingexample
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.request.RequestOptions
+import java.text.SimpleDateFormat
+import java.util.*
 
-@BindingAdapter("imageUrl")
-fun setImageUrl(imageView: ImageView, url: String) {
+@BindingAdapter("imageUrl", "circle", requireAll = false)
+fun setImageUrl(imageView: ImageView, url: String, circle: Boolean = true) {
+
+    val requestOptions =
+        if (circle)
+            RequestOptions.circleCropTransform()
+        else
+            RequestOptions.centerCropTransform()
+
     GlideApp.with(imageView.context)
         .load(url)
-        .apply(RequestOptions.circleCropTransform())
+        .apply(requestOptions)
         .placeholder(R.drawable.rounded_photo)
         .into(imageView)
+}
+
+@BindingAdapter("date", "dateFormat")
+fun dateFormat(textView: TextView, date: Date, format: String) {
+    textView.text = SimpleDateFormat(format).format(date)
 }
