72459ea57899da3969c07a0cdd48f84340fbf1d2
Daniel Sanchez Ceinos <daniel.sanchezc@masmovil.com>
2019-04-10 12:26:09 +0200

    Step 5

--- app/src/main/java/com/danielsanchezc/profiledatabindingexample/BindingAdapters.kt
+++ app/src/main/java/com/danielsanchezc/profiledatabindingexample/BindingAdapters.kt
@@ -1,14 +1,30 @@
 package com.danielsanchezc.profiledatabindingexample
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.request.RequestOptions
+import java.text.SimpleDateFormat
+import java.util.*
 
-@BindingAdapter("imageUrl")
-fun setImageUrl(imageView: ImageView, url: String) {
+@BindingAdapter("imageUrl", "circle", requireAll = false)
+fun setImageUrl(imageView: ImageView, url: String, circle: Boolean = true) {
+
+    val requestOptions =
+            if (circle)
+                RequestOptions.circleCropTransform()
+            else
+                RequestOptions.centerCropTransform()
+
     GlideApp.with(imageView.context)
-        .load(url)
-        .apply(RequestOptions.circleCropTransform())
-        .placeholder(R.drawable.rounded_photo)
-        .into(imageView)
+            .load(url)
+            .apply(requestOptions)
+            .placeholder(R.drawable.rounded_photo)
+            .into(imageView)
+}
+
+@BindingAdapter("date", "dateFormat")
+fun dateFormat(textView: TextView, date: Date?, format: String) {
+    if (date == null) return
+    textView.text = SimpleDateFormat(format).format(date)
 }
--- app/src/main/java/com/danielsanchezc/profiledatabindingexample/ProfileActivity.kt
+++ app/src/main/java/com/danielsanchezc/profiledatabindingexample/ProfileActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.danielsanchezc.profiledatabindingexample.databinding.ProfileActivityBinding
-import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -20,23 +19,22 @@ class ProfileActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.profile_activity)
 
         val guybrushProfile = Profile(
-            firstName = "Guybrush",
-            middleName = "Ulises",
-            lastName = "Threepwood",
-            bornDate = GregorianCalendar().apply {
-                set(GregorianCalendar.YEAR, 1687)
-                set(GregorianCalendar.MONTH, 2)
-                set(GregorianCalendar.DATE, 29)
-            }.time,
-            deathDate = GregorianCalendar().apply {
-                set(GregorianCalendar.YEAR, 2013)
-                set(GregorianCalendar.MONTH, 4)
-                set(GregorianCalendar.DATE, 3)
-            }.time,
-            photoUrl = "https://i.pinimg.com/originals/7f/dc/45/7fdc45bf7b245b896c12f1379750fe6e.jpg"
+                firstName = "Guybrush",
+                middleName = "Ulises",
+                lastName = "Threepwood",
+                bornDate = GregorianCalendar().apply {
+                    set(GregorianCalendar.YEAR, 1687)
+                    set(GregorianCalendar.MONTH, 2)
+                    set(GregorianCalendar.DATE, 29)
+                }.time,
+                deathDate = GregorianCalendar().apply {
+                    set(GregorianCalendar.YEAR, 2013)
+                    set(GregorianCalendar.MONTH, 4)
+                    set(GregorianCalendar.DATE, 3)
+                }.time,
+                photoUrl = "https://i.pinimg.com/originals/7f/dc/45/7fdc45bf7b245b896c12f1379750fe6e.jpg"
         )
 
-        binding.dateFormatter = SimpleDateFormat("yyyy")
         binding.profile = guybrushProfile
         binding.onProfilePhotoClick = View.OnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply { data = Uri.parse(guybrushProfile.photoUrl) })
